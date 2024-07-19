a96e9a750d19db1a69a58e9a3cf7ce41abf3877b
Gourang Patidar <goourangpatidar2003@gmail.com>
2024-07-19 14:45:22 -0700

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ project_1_page = st.Page(
 )
 project_2_page = st.Page(
     "views/chatbot.py",
-    title="Chat Bot",
+    title="GoGPT",
     icon=":material/smart_toy:",
 )
 
--- views/chatbot.py
+++ views/chatbot.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from openai import OpenAI
+st.header("GoGPT" , divider="rainbow")
 
 # Show title and description.
 openai_api_key=st.secrets["OPENAI_API_KEY"]
