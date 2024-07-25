68eca0cb5175609ed45f5deef0507f09d4a35cb3
Gourang Patidar <110373782+GourangPatidar@users.noreply.github.com>
2024-07-25 14:43:51 +0530

    Update quiz.py

--- views/quiz.py
+++ views/quiz.py
@@ -1,6 +1,6 @@
 import json
 from langchain.llms import OpenAI
-from opeai import OpenAI
+from openai import OpenAI
 from langchain import LLMChain, PromptTemplate
 from PyPDF2 import PdfReader
 import streamlit as st
