4d3aaa08bcb4ea7b919d0602b21502e815a65392
Gourang Patidar <goourangpatidar2003@gmail.com>
2024-07-28 00:06:19 +0530

    structure channged

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ project_6_page = st.Page(
 pg = st.navigation(
     {
         "INFO": [about_page],
-        "PROJECTS": [project_1_page, project_2_page, project_3_page , project_4_page , project_5_page , project_6_page],
+        "PROJECTS": [project_6_page, project_1_page, project_2_page , project_3_page , project_4_page , project_5_page],
     }
 )
 
