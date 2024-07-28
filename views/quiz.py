b9ca03cc157c50bfd21767629d3619b52a52a9c1
Gourang Patidar <goourangpatidar2003@gmail.com>
2024-07-28 14:41:07 +0530

    blog updated

--- views/quiz.py
+++ views/quiz.py
@@ -43,6 +43,7 @@ def extract_text_from_url(url):
         soup = BeautifulSoup(response.content, 'html.parser')
         paragraphs = soup.find_all('p')
         text = '\n'.join([p.get_text() for p in paragraphs])
+        text=text[20:-20]
     else:
         text = ""  # Handle other types of URLs as needed
     return text
