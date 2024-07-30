7e11d4b7b421ea42be8bde777bfa1156f57e184f
Gourang Patidar <goourangpatidar2003@gmail.com>
2024-07-30 14:30:02 +0530

    pdf updated

--- views/quiz.py
+++ views/quiz.py
@@ -288,23 +288,25 @@ if 'questions' in st.session_state:
 
         pdf = FPDF()
         pdf.add_page()
-        pdf.set_font("Arial", size=12)
-        pdf.cell(200, 10, txt="Quiz Questions", ln=True, align="C")
+        pdf.set_font("Arial","BU", size=16)
+        pdf.cell(0, 10, txt="Quiz Questions", ln=True, align="C")
 
         pdf.cell(0, 10, txt=school_name, ln=True, align="C")
         pdf.cell(0, 10, txt=exam_title, ln=True, align="C")
         pdf.cell(0, 10, txt=subject, ln=True, align="C")
         pdf.cell(0, 10, txt="", ln=True)  # Add an empty line after header
-        pdf.set_font("Arial", size=12)
+        pdf.set_font("Arial","B", size=12)
 
         # Add header information (smaller text)
         pdf.cell(0, 10, txt=f"STD - VII", ln=True, align="L")
         pdf.cell(0, 10, txt=f"TIME : {exam_time}", ln=True, align="C")
         pdf.cell(0, 10, txt=f"MAXIMUM MARKS {total_marks}", ln=True, align="R")
-        pdf.cell(0, 10, txt="", ln=True)  # Add an empty line after header
+        pdf.cell(0, 10, txt="---------------------------------------------------------------", ln=True)  # Add an empty line after header
 
         for idx, question in enumerate(st.session_state.questions, start=1):
+            pdf.set_font("Arial", 'B', size=12)
             pdf.cell(0, 10, txt=f"Q{idx}: {question['question']}", ln=True)
+            pdf.set_font("Arial", size=12)
             if question['type'] in ["single_select", "multiple_select", "true_false"]:
                 for option in question['options']:
                     pdf.cell(0, 10, txt=f"- {option}", ln=True)
@@ -321,7 +323,7 @@ if 'questions' in st.session_state:
         # Generate PDF with questions and answers
         pdf_answers = FPDF()
         pdf_answers.add_page()
-        pdf_answers.set_font("Arial", size=12)
+        pdf_answers.set_font("Arial","BU", size=16)
         pdf_answers.cell(200, 10, txt="Quiz Questions with Answers", ln=True, align="C")
 
         
@@ -330,7 +332,7 @@ if 'questions' in st.session_state:
         pdf_answers.cell(0, 10, txt=exam_title, ln=True, align="C")
         pdf_answers.cell(0, 10, txt=subject, ln=True, align="C")
         pdf_answers.cell(0, 10, txt="------------------------------------------------------------------------------", ln=True)  # Add an empty line after header
-        pdf_answers.set_font("Arial", size=12)
+        pdf_answers.set_font("Arial","B", size=12)
 
         # Add header information (smaller text)
         pdf_answers.cell(0, 10, txt=f"STD - VII", ln=True, align="L")
@@ -340,7 +342,9 @@ if 'questions' in st.session_state:
 
 
         for idx, question in enumerate(st.session_state.questions, start=1):
+            pdf.set_font("Arial", 'B', size=12)
             pdf_answers.cell(0, 10, txt=f"Q{idx}: {question['question']}", ln=True)
+            pdf.set_font("Arial", size=12)
             if question['type'] in ["single_select", "multiple_select", "true_false"]:
                 for option in question['options']:
                     pdf_answers.cell(0, 10, txt=f"- {option}", ln=True)
