1687d5416d7c3682a7f671de0dcadb555ca865f6
Gourang Patidar <goourangpatidar2003@gmail.com>
2024-08-05 00:08:21 +0530

    updated portfolio

--- views/portfolio.py
+++ views/portfolio.py
@@ -82,17 +82,7 @@ for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
 
 
 
-# --- EXPERIENCE & QUALIFICATIONS ---
-st.write('\n')
-st.subheader("Experience", divider="rainbow")
-st.write(
-    """
-- ✔️ 7 Years expereince extracting actionable insights from data
-- ✔️ Strong hands on experience and knowledge in Python and Excel
-- ✔️ Good understanding of statistical principles and their respective applications
-- ✔️ Excellent team-player and displaying strong sense of initiative on tasks
-"""
-)
+
 
 
 # --- SKILLS ---
